7e09f4585b10d19d63d27674f5aeb3b9a5f56866
Heike Winkelvoß <public@egladil.de>
2019-10-04 13:46:15 +0200

    NotFound exception fixed

--- src/main/java/de/egladil/web/quarkus_hello/exception/GlobalExceptionHandler.java
+++ src/main/java/de/egladil/web/quarkus_hello/exception/GlobalExceptionHandler.java
@@ -26,38 +26,20 @@ public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
 	@Override
 	public Response toResponse(final Throwable exception) {
 
-		log.info("caught this exception: {}", exception.getClass().getName());
-
-		if (exception.getCause() != null) {
-
-			log.info("the wrapped exception is: {}", exception.getCause().getClass().getName());
-		}
-
-		Throwable theRealException = exception;
-
-		if (exception.getCause() != null) {
-
-			theRealException = exception.getCause();
-		}
-
-		return handleTheRealException(theRealException);
-	}
-
-	private Response handleTheRealException(final Throwable exception) {
-
-		if (exception instanceof ConnectException) {
+		if (exception instanceof ConnectException
+			|| exception.getCause() != null && exception.getCause() instanceof ConnectException) {
 
 			return Response.status(502).entity("Der authprovider ist nicht erreichbar").build();
 		}
 
-		if (exception instanceof NotFoundException) {
+		if (exception instanceof NotFoundException
+			|| exception.getCause() != null && exception.getCause() instanceof NotFoundException) {
 
 			return Response.status(404).entity("Diese URL kennen wir nicht (HEX)").build();
 		}
 
 		log.error(exception.getMessage(), exception);
 		return Response.serverError().entity("OMG!!! Da ist wohl irgend etwas schiefgelaufen :/").build();
-
 	}
 
 }
